986a95ed0469b804f1b352f44dfd08042233a10d
Veerachart Srisamosorn <veerachart.sr@gmail.com>
2016-11-08 19:30:52 +0900

    Add PID controller to control blimp in thrust direction. To be tested.

--- src/controller.cpp
+++ src/controller.cpp
@@ -1,21 +1,74 @@
 #include <ros/ros.h>
 #include <math.h>
 #include <tf/transform_broadcaster.h>
+#include <tf/transform_listener.h>
 #include <dynamic_reconfigure/server.h>
 #include <blimp_controller/ControllerConfig.h>
+#include <string>
+#include <vector>
+#include <algorithm>
 
+class PID {
+    private:
+        float kp, ki, kd;
+        float current_u;
+        float prev_u;
+        float current_e;
+        float prev_e_1;
+        float prev_e_2;
+        ros::Time current;
+        ros::Time last;
+        double dt;
+        
+    public:
+        PID (float kp_, float ki_, float kd_) {
+            kp = kp_;
+            ki = ki_;
+            kd = kd_;
+            prev_u = 0;
+            prev_e_1 = 0;
+            prev_e_2 = 0;
+            last = ros::Time::now();
+        }
+        
+        float update (float e_) {
+            current = ros::Time::now();
+            current_e = e_;
+            dt = (current - last).toSec();
+            current_u = prev_u + kp*(current_e-prev_e_1) + ki*dt*current_e + kd/dt*(current_e-2*prev_e_1+prev_e_2);
+            
+            // TODO saturation
+
+            // Store data as previous
+            prev_u = current_u;
+            prev_e_2 = prev_e_1;
+            prev_e_1 = current_e;
+            
+            return current_u;
+        }
+        
+        void tune_pid (float kp_, float ki_, float kd_) {
+            kp = kp_;
+            ki = ki_;
+            kd = kd_;
+        }
+};
 
 class BlimpController {
     private:
         ros::NodeHandle nh_;
         dynamic_reconfigure::Server<blimp_controller::ControllerConfig> server;
         dynamic_reconfigure::Server<blimp_controller::ControllerConfig>::CallbackType f;
+        tf::TransformListener listener;
+        PID *pid_thrust;
+        float command_thrust;
     public:
         BlimpController(ros::NodeHandle &node_)
         {
             nh_ = node_;
             f = boost::bind(&BlimpController::callback, this, _1, _2);
             server.setCallback(f);
+            pid_thrust = new PID(1.0, 0.0, 1.0);
         }
         void callback(blimp_controller::ControllerConfig &config, uint32_t level){
             if(config.go){
@@ -28,13 +81,48 @@ class BlimpController {
                 br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "world", "goal"));
             }
         }
+        
+        void navigate(){
+            std::vector<std::string> frames;
+            tf::StampedTransform transform;
+            ros::Time last_time;
+            std::string *error;
+            try{
+                listener.getFrameStrings(frames);
+                if (std::find(frames.begin(), frames.end(), "blimp") != frames.end()){
+                    listener.getLatestCommonTime("world","blimp",last_time,error);
+                    if (ros::Time::now()-last_time > ros::Duration(1))
+                        return;         // Too old
+                    listener.lookupTransform("blimp", "goal", ros::Time(0), transform);
+                    
+                    // TODO align orientation to the shortest part.
+                    // transform.getOrientation() --> Quaternion
+                    
+                    float e_x = transform.getOrigin().x();
+                    float e_y = transform.getOrigin().y();
+                    float e_z = transform.getOrigin().z();
+                    
+                    command_thrust = pid_thrust->update(e_x);
+                    ROS_INFO("%.2f", command_thrust);
+                    
+                }
+            }
+            catch (tf::TransformException ex){
+                ROS_ERROR("%s", ex.what());
+            }
+        }
 };
 
 int main (int argc, char **argv) {
     ros::init(argc, argv, "blimp_controller");//, ros::init_options::AnonymousName);
 
     ros::NodeHandle nh;
+    ros::Rate rate(30);
     BlimpController blimp_controller(nh);
-    ros::spin();
+    while (nh.ok()){
+        blimp_controller.navigate();
+        ros::spinOnce();
+        rate.sleep();
+    }
     return 0;
 }
